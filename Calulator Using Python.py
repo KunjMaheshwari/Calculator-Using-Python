fcf13e5e6c38f2c6fb6eee59155bba1fcd5e49be
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2022-11-16 22:49:54 +0530

    Update Calulator Using Python.py

--- Calulator Using Python.py
+++ Calulator Using Python.py
@@ -1,20 +1,51 @@
-#Calculator Using Python 
+# Calculator using Python
 
-#Take Two numbers as an input from the user 
-num_1 = input("Enter the number 1: ")
-num_2 = input("Enter the number 2: ")
+# define all the operations
+def addition(x, y):
+    return x+y
 
-#Enter the Operation you want to perform 
-operation = input("Enter the operation: ")
 
-if operation == "+":
-    print (int(num_1) + int(num_2) )
-elif operation == "-":
-    print (int(num_1) - int(num_2))
-elif operation == "*":
-    print (int(num_1) * int(num_2))
-elif operation == "/":
-    print (int(num_1) / int(num_2))
-else:
-    print ("Enter the valid Operation")
-    
+def substraction(x, y):
+    return x-y
+
+
+def division(x, y):
+    return x/y
+
+
+def multiplication(x, y):
+    return x*y
+
+
+# exaplin the operations using dictionary
+operation = {
+    "+": addition,
+    "-": substraction,
+    "/": division,
+    "*": multiplication
+}
+
+
+def calculator():                                     # Recursion function(A function that calls itself)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operation:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+
+        operation_symbol = input("Pick an operation from the line above: ")
+        num = float(input("Enter the next number: "))
+        calculation_function = operation[operation_symbol]
+        answer = calculation_function(num1, num)
+
+        print(f"{num1} {operation_symbol} {num} = {answer}")
+
+        if (input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y'):
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
